e12dcd441a64e02a91deeabfc954a594a73cfc49
dharshatharan <dharshatharan@gmail.com>
2021-07-11 10:19:41 -0400

    Add Fathom Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,11 @@ import "@styles/globals.css";
 
 import { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { useAnalytics } from "@lib/analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useAnalytics();
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
