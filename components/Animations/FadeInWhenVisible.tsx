6eed44b7250042da22818b67ac58acb782b5e0ce
dharshatharan <dharshatharan@gmail.com>
2021-08-25 06:52:31 -0400

    Change to fade in from bottom

--- components/Animations/FadeInWhenVisible.tsx
+++ components/Animations/FadeInWhenVisible.tsx
@@ -21,10 +21,10 @@ export default function FadeInWhenVisible({ children }: Props): ReactElement {
       ref={ref}
       animate={controls}
       initial="hidden"
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.5 }}
       variants={{
-        visible: { opacity: 1, x: 0 },
-        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, y: 0 },
+        hidden: { opacity: 0, y: 100 },
       }}
     >
       {children}
