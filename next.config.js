24fe11ff741a01178115877cf3acd511d69a8f6f
dharshatharan <dharshatharan@gmail.com>
2021-12-08 11:56:15 -0500

    Sentry issue workaround

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// const { withSentryConfig } = require("@sentry/nextjs");
+const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
   images: {
@@ -11,19 +11,21 @@ const moduleExports = {
   },
 };
 
-// const SentryWebpackPluginOptions = {
-//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
-//   // the following options are set automatically, and overriding them is not
-//   // recommended:
-//   //   release, url, org, project, authToken, configFile, stripPrefix,
-//   //   urlPrefix, include, ignore
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
-//   silent: true, // Suppresses all logs
-//   // For all available options, see:
-//   // https://github.com/getsentry/sentry-webpack-plugin#options.
-// };
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+
+  // to fix sentrt crashing issue
+  outputFileTracing: false,
+};
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-// module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
-module.exports = moduleExports;
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
